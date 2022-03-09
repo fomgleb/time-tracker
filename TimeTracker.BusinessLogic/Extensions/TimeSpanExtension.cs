170744e2f5b3297d5f1a24002f07b18276bc4b9a
GLEB <fomglib@gmail.com>
2022-03-09 17:36:51 +0200

    Add extention ToStringWithoutDays for TimeSpan.

---
+++ TimeTracker.BusinessLogic/Extensions/TimeSpanExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.BusinessLogic.Extensions
+{
+    public static class TimeSpanExtension
+    {
+        public static string ToStringWithoutDays(this TimeSpan timeSpan)
+        {
+            var seconds = timeSpan.Seconds > 9 ? timeSpan.Seconds.ToString() : $"0{timeSpan.Seconds}";
+            var minutes = timeSpan.Minutes > 9 ? timeSpan.Minutes.ToString() : $"0{timeSpan.Minutes}";
+            var hours = timeSpan.Hours + timeSpan.Days * 24 > 9 ? (timeSpan.Hours + timeSpan.Days * 24).ToString() : $"0{timeSpan.Hours}";
+
+            return $"{hours}:{minutes}:{seconds}";
+        }
+    }
+}
