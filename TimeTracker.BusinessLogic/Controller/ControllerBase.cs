f7826a85126bdedc4b04b69c496a0bfd5b2af69c
GLEB <fomglib@gmail.com>
2022-03-10 17:54:24 +0200

    Add ControllerBase not to repeat methods - Save and Load.

--- TimeTracker.BusinessLogic/Controller/ControllerBase.cs
+++ TimeTracker.BusinessLogic/Controller/ControllerBase.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TimeTracker.BusinessLogic.Controller
 {
-    internal class ControllerBase
+    public abstract class ControllerBase
     {
+        /// <summary>
+        /// Save data to file.
+        /// </summary>
+        /// <param name="fileName"> The name of the file where the data will be saved. </param>
+        /// <param name="item"> Object to save. </param>
+        public void Save(string fileName, object item)
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, item);
+            }
+        }
+
+        /// <summary>
+        /// Returns loaded data or default value for T.
+        /// </summary>
+        /// <typeparam name="T"> Type of loading data. </typeparam>
+        /// <param name="fileName"> The name of the file from which the data will be loaded. </param>
+        protected T Load<T>(string fileName)
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                if (fileStream.Length > 0 && formatter.Deserialize(fileStream) is T items)
+                    return items;
+                return default;
+            }
+        }
     }
 }
